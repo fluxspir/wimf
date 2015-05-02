a0b08a423b101721062d05ab418ecdb39574af31
Franck Labadille <franck@kernlog.net>
2015-05-02 16:23:09 +0200

    adding a vault

---
+++ wimf/commands/vault.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from base import BaseCommand
+import models
+
+class AddVault(BaseCommand):
+    """  """
+    command_name = "add_vault"
+    
+    def __init__(self):
+       self.values = {}
+
+    def run(self, values):
+        self.values = values
+        new_vault = models.Vault(**self.values)
+        models.session.add(new_vault)
+        models.session.commit()
+        
--- wimf/test.py
+++ wimf/test.py
@@ -3,42 +3,80 @@
 import pdb
 import models
 import commands
+import sqlalchemy
 
-def test_model():
+def test_model(True):
     try:
         if models.session:
-            print("session object exists")
+            print("Success: session object exists")
     except:
         raise
 
-def test_add():
-    # add a keyword
-    values = { "keyword": "Test_add_keyword" }
-    add_keyword = commands.misc.AddKeyword()
-    try:
-        add_keyword.run(values)
-        print("new keyword {} added to __keyword__ table".format(
-                                                            values["keyword"]))
-    except:
-        raise
+def test_add_keyword_geolocation(test=False):
+    if test:
+        # add a keyword
+        values = { "keyword": "Test_add_keyword" }
+        add_keyword = commands.misc.AddKeyword()
+        try:
+            add_keyword.run(values)
+            print("Success: new keyword {} added to __keyword__ table".format(
+                                                        values["keyword"]))
+        except:
+            raise
 
-    # add a geolocation
+        # add a geolocation
+        if True:
+            test_values = [
+                { "name": "" , "gps": "" },
+                { "name": "Paris", "gps": "" },
+                { "name": "Paris", "gps": "43.3 ; 120.6" },
+                { "name": "" , "gps": "52N48'33'' , 018E12'30''" }
+                ]
+            add_geo = commands.misc.AddGeoLocation()
+
+        if True:
+            for values in test_values:
+                try:
+                    add_geo.run(values)
+                    print("Success: added (name {}, gps {} in __geolocation__ 
+                                table".format(values["name"], values["gps"]))
+                except:
+                    raise
+
+def test_add_vault(test):
+    # add a vault [ ( values={}, msg), ]
     test_values = [
-        { "name": "" , "gps": "" },
-        { "name": "Paris", "gps": "" },
-        { "name": "Paris", "gps": "43.3 ; 120.6" },
-        { "name": "" , "gps": "52N48'33'' , 018E12'30''" }
+        ( { "url": None, "name": None, "geolocation": None, "price": None }, 
+                                            "Success: Vault refuse null url"),
+        ( { "url": "/dev/null", 
+                    "name": "trash", "geolocation": None, "price": None },
+                    "Success: Vault accept local path {}".format("/dev/null")),
+        ( { "url": "franck@prometheus.tamentis.com", 
+            "name": "test_price_num", 
+            "geolocation": 1, 
+            "price": 12.3 },
+            "Success: Vault accept price numeric and geoloc.id"),
+        ( { "url": "franck@prometheus.tamentis.com", 
+            "name": "test_price_num", 
+            "geolocation": None, "price": None},
+            "Success: Vault refuse double entry url-name"),
         ]
-    add_geo = commands.misc.AddGeoLocation()
-    try:
-        for values in test_values:
-            add_geo.run(values)
-            print("added (name {}, gps {} in __geolocation__ table".format(
-                                            values["name"], values["gps"]))
-    except:
-        raise
+    add_vault = commands.vault.AddVault()
+    for (values, msg) in test_values:
+        try:
+            add_vault.run(values)
+            print(msg)
+        except sqlalchemy.exc.StatementError:
+            if not values["url"]:
+                models.session.rollback()
+                print(msg)
+            else:
+                raise
+
 
 
 if __name__ == "__main__":
-    test_model()
-    test_add()
+    test_model(True)
+    test_add_keyword_geolocation(True)
+    test_add_vault(True)
+
