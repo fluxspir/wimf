85d483ce792ddda63271dccab85ca9b9abb0acc5
Franck Labadille <franck@kernlog.net>
2015-05-02 13:33:37 +0200

    test add keyword

--- wimf/test.py
+++ wimf/test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+import pdb
 import models
+import commands
 
 def test_model():
     try:
@@ -9,6 +11,17 @@ def test_model():
     except:
         raise
 
+def test_add():
+    try:
+        add_keyword = commands.misc.AddKeyword()
+        values = { "keyword": "Test_add_keyword" }
+        add_keyword.run(values)
+        print("new keyword {} added to __keyword__ table".format(
+                                                            values["keyword"]))
+    except:
+        raise
+
 
 if __name__ == "__main__":
     test_model()
+    test_add()
